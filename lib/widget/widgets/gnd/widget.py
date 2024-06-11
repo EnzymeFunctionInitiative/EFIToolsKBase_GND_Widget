923b69ee70d6b79cbba27bdf8137a5d5d503064e
Sahas Ramesh <>
2024-06-11 10:49:15 -0700

    works for direct jobs, ready to be registered as a service

--- lib/widget/widgets/gnd/widget.py
+++ lib/widget/widgets/gnd/widget.py
@@ -1,4 +1,3 @@
-import re
 from widget.lib.widget_base import WidgetBase
 import sqlite3
 
@@ -7,24 +6,35 @@ class GndParams:
 		# the P object
 		self.P = {}
 		# from the query string
-		self.db = params.get("gnn-id", params.get("direct-id", "")) + ".sqlite"
-		self.my_id = params.get("gnn-id", params.get("direct-id", ""))
-		self.gnn_key = params.get("key", "")
-		self.gnn_name = "job #" + self.my_id
-		self.gnn_title = self.gnn_name
-		self.gnn_download_name = self.my_id
-		self.uniref_version = params.get("id-type", "")
-		self.uniref_id = params.get("uniref-id", "")
+		self.db = params.get("gnn-id", params.get("direct-id", params.get("upload-id", ""))) + ".sqlite"
+		self.P["is_uploaded_diagram"] = "upload-id" in params
+		self.P["gnn_id"] = params.get("gnn-id", params.get("direct-id", ""))
+		self.P["gnn_key"] = params.get("key", "")
+		self.P["gnn_name"] = "job #" + self.P["gnn_id"]
+		self.P["gnn_title"] = self.P["gnn_name"]
+		self.P["gnn_download_name"] = str(self.P["gnn_id"]) + "_"
+		self.P["uniref_version"] = params.get("id-type", "")
+		self.P["uniref_id"] = params.get("uniref-id", "")
 
 		# from the database
-		self.gnn_window = 10
-		self.gnn_type = ""
+		self.P["nb_size"] = 10
+		self.P["gnn_type"] = ""
+		self.P["supports_download"] = True
+		self.P["is_blast"] = False
 
-		self.has_unmatched_ids = False
-		self.unmatched_ids = []
-		self.unmatched_id_modal_text = ""
+		# unmatched ids
+		self.P["has_unmatched_ids"] = False
+		self.P["unmatched_ids"] = []
+		self.P["unmatched_id_modal_text"] = ""
 
-		self.is_direct_job = True if "direct-id" in params and "type" in params else False
+		# uniprot ids
+		# blast sequence
+
+		# things that have to be calculated
+		self.P["is_direct_job"] = True if "direct-id" in params and "type" in params else False
+
+		# not important
+		self.P["is_example"] = False
 
 	def fetch_data(self, query):
 		conn = sqlite3.connect(self.db)
@@ -47,7 +57,7 @@ class GndParams:
 	def get_realtime_params(self):
 		self.P["id_key_query_string"] = "mode=rt"
 		self.P["gnn_name_text"] = "A"
-		self.P["window_title"] = ""
+		self.P["nb_size_title"] = ""
 		self.P["is_realtime_job"] = True
 		self.P["gnn_id"] = -1
 		self.P["gnn_key"] = ""
@@ -79,75 +89,51 @@ class GndParams:
 		else:
 			ids = self.get_ids_from_match_table()
 		return ids
-	
-	def set_is_direct_job(self):
-		pass
 
 	def retrieve_info(self):
 		name = self.fetch_data("SELECT name FROM metadata")[0][0]
 		if name != None and name != "":
-			self.gnn_name = "<i>" + name + "</i>"
-			self.gnn_title = name + " #(" + self.my_id + ")"
-			self.gnn_download_name += "_" + name
+			self.P["gnn_name"] = "<i>" + name + "</i>"
+			self.P["gnn_title"] = name + " #(" + self.P["gnn_id"] + ")"
+			self.P["gnn_download_name"] += name
 
-		window = self.fetch_data("SELECT neighborhood_size FROM metadata")[0][0]
-		if window != None and window != "":
-			self.gnn_window = window
+		nb_size = self.fetch_data("SELECT neighborhood_size FROM metadata")[0][0]
+		if nb_size != None and nb_size != "":
+			self.P["nb_size"] = nb_size
 
 		type = self.fetch_data("SELECT type FROM metadata")[0][0]
-		if type != None and type != "" and type == "BLAST":
-			self.gnn_type = "Sequence BLAST"
-		elif type != None and type != "" and type == "FASTA":
-			self.gnn_type = "FASTA header ID lookup"
-		elif type != None and type != "" and type == "ID_LOOKUP":
-			self.gnn_type = "Sequence ID lookup"
-		elif type != None and type != "" and type == "gnn":
-			self.gnn_type = "GNN"
+		if type == "BLAST":
+			self.P["gnn_type"] = "Sequence BLAST"
+			self.P["is_blast"] = True
+		elif type == "FASTA":
+			self.P["gnn_type"] = "FASTA header ID lookup"
+		elif type == "ID_LOOKUP":
+			self.P["gnn_type"] = "Sequence ID lookup"
+		elif type == "gnn":
+			self.P["gnn_type"] = "GNN"
 		else:
-			# TODO: else what is the type??
-			self.is_direct_job = False
+			self.P["is_direct_job"] = False
 
-		self.has_unmatched_ids = self.check_table_exists("unmatched")
-		if self.has_unmatched_ids:
+		self.P["has_unmatched_ids"] = self.check_table_exists("unmatched")
+		if self.P["has_unmatched_ids"]:
 			column = self.fetch_data("SELECT id_list FROM unmatched")
 			for val in column:
-				self.unmatched_ids.append(val[0])
-				self.unmatched_id_modal_text += "<div>" + val[0] + "</div>"
+				self.P["unmatched_ids"].append(val[0])
+				self.P["unmatched_id_modal_text"] += "<div>" + val[0] + "</div>"
 
-		self.uniprot_ids = self.get_uniprot_ids()
+		self.P["uniprot_ids"] = self.get_uniprot_ids()
 		content = "UniProt ID\tQuery ID\n"
-		for upId, otherId in self.uniprot_ids.items():
+		for upId, otherId in self.P["uniprot_ids"].items():
 			content += f"{upId}\t{otherId}\n"
-		self.uniprot_ids_modal_text = content
+		self.P["uniprot_ids_modal_text"] = content
 
-		res = {
-			"window": self.gnn_window,
-			"type": self.gnn_type,
-			"id": self.my_id,
-			"key": self.gnn_key,
-			"name": self.gnn_name,
-			"title": self.gnn_title,
-			"download_name": self.gnn_download_name,
-			"has_unmatched_ids": self.has_unmatched_ids,
-			"unmatched_ids": self.unmatched_ids,
-			"unmatched_id_modal_text": self.unmatched_id_modal_text,
-			"uniprot_ids": self.uniprot_ids,
-			"uniprot_ids_modal_text": self.uniprot_ids_modal_text
-		}
-		print(res)
-		return res
+		self.P["blast_seq"] = self.fetch_data("SELECT sequence FROM metadata")[0][0]
 
-# class GNDFiles:
-#   def __init__(self, file_type):
-#     self.file_type = file_type
-# 		if file_type == "unmatched_ids":
-# 			params = GndParams(params)
-
-#   def download_file(self):
+		return self.P
 
 class Widget(WidgetBase):
 	def context(self):
-		possible_params = ['direct-id', 'gnn-id', 'key', 'id-type', 'uniref-id']
+		possible_params = ["direct-id", "gnn-id", "key", "id-type", "uniref-id"]
 		params = {}
 		for param in possible_params:
 			if self.has_param(param):
